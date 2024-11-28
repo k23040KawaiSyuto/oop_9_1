9ed6b3300c02e9f38dbd9fe76103bfba7ad5859d
k23040KawaiSyuto <k23040kk@aitech.ac.jp>
2024-11-28 13:42:34 +0900

    あああ

---
+++  modules/SayHello.py
@@ -0,0 +1,10 @@
+class SayHello:
+    def__init__(self, target="World")
+    self.target = target
+    def say(self):
+        print(f"Hello, {self.target}!!")
+if__name__== '__main__'
+app = SayHello()
+app.say()
+app = SayHello("Someone")
+app.say()
---
+++ main.py
@@ -0,0 +1,6 @@
+import modules.SayHello as App
+def run():
+    app = App.SayHello("Git")
+    app.say()
+    if__name__= '__main__'
+    run()
